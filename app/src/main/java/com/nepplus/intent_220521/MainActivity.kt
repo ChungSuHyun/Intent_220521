09fd67bcc64f2981b9eb7f124f1acb7b69d4477a
정수현 <gng09087@naver.com>
2022-05-21 14:34:41 +0900

    other Activity 활용

--- app/src/main/java/com/nepplus/intent_220521/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_220521/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.nepplus.intent_220521
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMove.setOnClickListener {
+
+//            다른 화면으로 이동하기
+
+            val myIntent = Intent(this,OtherActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/nepplus/intent_220521/OtherActivity.kt
@@ -0,0 +1,13 @@
+package com.nepplus.intent_220521
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+
+    btnMove
+}
